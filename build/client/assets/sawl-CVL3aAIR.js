dca8c21a9609a5a515c291acfd4bfb9a2919dcdf
biodun-dev <abdultoheeb97@gmail.com>
2025-02-16 22:06:01 +0100

    initialize

--- build/client/assets/sawl-CVL3aAIR.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/sawl-DNfXtAl-.jpg",e="/assets/spr-lesson-builder-dark-placeholder-BYjrS8rr.jpg",r="/assets/sawl-DNfXtAl-.jpg";export{r as a,s as b,e as s};
--- server.js
+++
@@ -1,8 +0,0 @@
-import { createRequestHandler } from "@remix-run/netlify";
-
-export default createRequestHandler({
-  getLoadContext() {
-    // Optional: Provide custom context to loaders
-    return {};
-  },
-});
