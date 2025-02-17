677c7733b56db390e4a639845dadeaee70839ffc
biodun-dev <abdultoheeb97@gmail.com>
2025-02-17 11:14:50 +0100

    fix

--- app/routes/contact/contact.jsx
+++ app/routes/contact/contact.jsx
@@ -1,21 +1,23 @@
-import { Button } from '~/components/button';
-import { DecoderText } from '~/components/decoder-text';
-import { Divider } from '~/components/divider';
-import { Footer } from '~/components/footer';
-import { Heading } from '~/components/heading';
-import { Icon } from '~/components/icon';
-import { Input } from '~/components/input';
-import { Section } from '~/components/section';
-import { Text } from '~/components/text';
-import { tokens } from '~/components/theme-provider/theme';
-import { Transition } from '~/components/transition';
-import { useFormInput } from '~/hooks';
-import { useRef } from 'react';
-import { cssProps, msToNum, numToMs } from '~/utils/style';
-import { baseMeta } from '~/utils/meta';
-import { Form, useActionData, useNavigation } from '@remix-run/react';
-import { json } from '@remix-run/cloudflare';
+import dotenv from 'dotenv';
+dotenv.config();
 import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
+import { json } from '@remix-run/node';
+import { Form, useActionData, useNavigation } from '@remix-run/react';
+import { useRef } from 'react';
+import { useFormInput } from '../../hooks';
+import { Button } from '../../components/button';
+import { DecoderText } from '../../components/decoder-text';
+import { Divider } from '../../components/divider';
+import { Footer } from '../../components/footer';
+import { Heading } from '../../components/heading';
+import { Icon } from '../../components/icon';
+import { Input } from '../../components/input';
+import { Section } from '../../components/section';
+import { Text } from '../../components/text'
+import { tokens } from '../../components/theme-provider/theme';
+import { Transition } from '../../components/transition';
+import { baseMeta } from '../../utils/meta';
+import { cssProps, msToNum, numToMs } from '../../utils/style';
 import styles from './contact.module.css';
 
 export const meta = () => {
@@ -34,8 +36,9 @@ export async function action({ context, request }) {
   const ses = new SESClient({
     region: 'us-east-1',
     credentials: {
-      accessKeyId: context.cloudflare.env.CUSTOM_AWS_ACCESS_KEY_ID,
-      secretAccessKey: context.cloudflare.env.CUSTOM_AWS_SECRET_ACCESS_KEY,
+      accessKeyId: process.env.CUSTOM_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.CUSTOM_AWS_SECRET_ACCESS_KEY,
+      
     },
   });
 
@@ -73,7 +76,7 @@ export async function action({ context, request }) {
   await ses.send(
     new SendEmailCommand({
       Destination: {
-        ToAddresses: [context.cloudflare.env.EMAIL],
+        ToAddresses: [process.env.EMAIL],
       },
       Message: {
         Body: {
@@ -85,7 +88,7 @@ export async function action({ context, request }) {
           Data: `Portfolio message from ${email}`,
         },
       },
-      Source: `Portfolio <${context.cloudflare.env.FROM_EMAIL}>`,
+      Source: `Portfolio <${process.env.FROM_EMAIL}>`,
       ReplyToAddresses: [email],
     })
   );
